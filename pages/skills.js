c87caabcd4c801f63054b2266ddd622e5961f699
Willwitcher <walberh96@gmail.com>
2022-11-24 23:20:18 -0500

    skills page kinda working

--- pages/skills.js
+++ pages/skills.js
@@ -1,12 +1,36 @@
-
+import Skill from "../components/Skill"
+import skillsStyle from '../styles/Skills.module.css'
+import DownloadIcon from '@mui/icons-material/Download';
+import { Button } from "@mui/material";
 const skills = () => {
   return (
-    <div>
+    <>
+    <div className={skillsStyle.skills}>
+    <h1>Programming Languages</h1>  
+    <Skill rank={5} nameSkill={'Java'}></Skill>
+    <Skill rank={5} nameSkill={'C#'}></Skill>
+    <Skill rank={5} nameSkill={'HTML'}></Skill>
+    <Skill rank={5} nameSkill={'CSS'}></Skill>
+    <Skill rank={5} nameSkill={'SQL'}></Skill>
+    <Skill rank={4} nameSkill={'JavaScript'}></Skill>
+    <Skill rank={4} nameSkill={'P.H.P'}></Skill>
+    <Skill rank={4} nameSkill={'Python'}></Skill>
+    <Skill rank={3} nameSkill={'C++'}></Skill>
+    <h1>Technologies</h1>  
+    <Skill rank={5} nameSkill={'Git&&Github'}></Skill>
+    <Skill rank={5} nameSkill={'Android Studio'}></Skill>
+    <Skill rank={5} nameSkill={'Next JS'}></Skill>
+    <Skill rank={5} nameSkill={'Unity Game Engine'}></Skill>
+    <h1>Spoken Languages</h1>  
+    <Skill rank={5} nameSkill={'Spanish'}></Skill>
+    <Skill rank={5} nameSkill={'English'}></Skill>
+    <Skill rank={3} nameSkill={'Japanese'}></Skill>
 
-        <h1>
-            Skills
-        </h1>
+    <Button variant='outlined' startIcon={<DownloadIcon></DownloadIcon>}></Button>
     </div>
+
+    </>
+
   )
 }
 
