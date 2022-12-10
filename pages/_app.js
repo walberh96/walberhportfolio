1530837dfc04c7fda7dc89ec6f9807df6328e5f2
Willwitcher <walberh96@gmail.com>
2022-12-10 16:02:22 -0500

    centered

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import NavResp from '../components/NavResp'
+import { Box } from '@mui/material'
+import { height, width } from '@mui/system'
 
 function MyApp({ Component, pageProps }) {
   return(
     <>
       <NavResp></NavResp>
+      <Box justifyContent="center" alignItems="center" display={"flex"}  m={1} sx={{ flexDirection: 'column' , marginInline:'auto' }} width={600}>
       <Component {...pageProps} />
+      </Box>
     </>
     
   ) 
