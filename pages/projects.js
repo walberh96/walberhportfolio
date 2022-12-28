b83896b26a200d77aed897c3675705cf76aad98a
Willwitcher <walberh96@gmail.com>
2022-12-27 20:04:53 -0500

    worldbox pictures added

--- pages/projects.js
+++ pages/projects.js
@@ -43,14 +43,36 @@ const onlinec_pictures = [
     title: 'Inventory in Game',
   },
 ];
-
+const worldbox_pictures = [
+  {
+    img: '../projects/worldbox_legends/1.png',
+    title: 'Main Menu',
+  },
+  {
+    img: '../projects/worldbox_legends/2.png',
+    title: 'Level Selection',
+  },
+  {
+    img: '../projects/worldbox_legends/3.png',
+    title: 'InGame1',
+  },
+  {
+    img: '../projects/worldbox_legends/4.png',
+    title: 'InGame2',
+  },
+  {
+    img: '../projects/worldbox_legends/5.png',
+    title: 'InGame3',
+  },
+];
 const projects = () => {
   return (
     <div>
       <Box justifyContent="center" alignItems="center" display={"flex"}  m={1} sx={{ flexDirection: 'column' }}>
         <Typography variant='h3' color='#5151ff' gutterBottom fontStyle={"italic"} fontFamily={"cursive"} >Projects</Typography>
         <Project name={"Online Chaos"} description={"This is an online R.P.G. Game been developed in Unity for PC and in future releases also mobile. We're using Mirror as the core for networking with some changes so that it can support players and network objects in multiple scenes at the same time."}  githubLink={'https://github.com/walberh96/'} images = {onlinec_pictures}></Project>
-        {/*<Project name={"WorldBox Legends 2.0"} description={"This is a puzzle game"}  githubLink={'https://github.com/walberh96'} images = {""}></Project>
+        <Project name={"WorldBox Legends 2.0"} description={"This is a puzzle game made in Unity, using Tilemaps with absolutely no colliders or physics."}  githubLink={'https://github.com/walberh96'} images = {worldbox_pictures}></Project>
+        {/*
         <Project name={"I'm Not Mario"} description={"This is a 2D classic game"}  githubLink={'https://github.com/walberh96'} images = {""}></Project>
         <Project name={"Facking Squares"} description={"This is a survival Top-Down Shooting game"} githubLink={'https://github.com/walberh96'} images = {""}></Project>
         <Project name={"Salti Soldiers"} description={"This is an online survival COOP Game"} githubLink={'https://github.com/walberh96'} images = {""}></Project>
